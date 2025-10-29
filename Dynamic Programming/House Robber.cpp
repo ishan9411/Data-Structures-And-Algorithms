717d4c027080c4ad88d3ae713a9cdab9c6f31cd1
isha9411 <ishan9411@gmail.com>
2025-10-30 01:02:13 +0530

    added bottom up for house robber

--- Dynamic Programming/House Robber.cpp
+++ Dynamic Programming/House Robber.cpp
@@ -24,3 +24,28 @@ public:
     }
 };
 
+//Solution 2 : Bottom-Up
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        int t[101];
+
+        //t[i] = max stolen money till i house
+
+        //no house
+        t[0] = 0;
+
+        //1 house
+        t[1] = nums[0];
+
+        for(int i = 2; i<=n; i++){
+            int steal = nums[i-1] + t[i-2];  //ye le lo or i-2 tk ka profit lelo
+            int skip  = t[i-1];              //i-1 tk ka profit lelo
+
+            t[i] = max(steal, skip);
+        }
+        return t[n];
+    }
+};
