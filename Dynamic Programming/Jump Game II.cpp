7608504f3081e5a4fc04600b559679c7fe854c2c
isha9411 <ishan9411@gmail.com>
2025-11-03 12:35:59 +0530

    added greedy for jump game II

--- Dynamic Programming/Jump Game II.cpp
+++ Dynamic Programming/Jump Game II.cpp
@@ -52,3 +52,27 @@ public:
         return t[n-1];
     }
 };
+
+//Soltuion 3 : Greedy Algorithm (Always checking farhtest)
+
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+
+        int l = 0;
+        int r = 0;
+        int jumps = 0;
+
+        while(r < n-1){
+            int farthest = 0;
+            for(int i = l; i<=r; i++){
+                farthest = max(i+nums[i], farthest);
+            }
+            l = r+1;
+            r = farthest;
+            jumps++;
+        }
+        return jumps;
+    }
+};
