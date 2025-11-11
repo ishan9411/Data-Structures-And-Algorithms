e9e9d22cde75e248aebd3c92825ca3dfec014be2
isha9411 <ishan9411@gmail.com>
2025-11-11 10:28:50 +0530

    added ReverseArray

---
+++ Array/ReverseArray.cpp
@@ -0,0 +1,13 @@
+//Given an array/list 'ARR' of integers and a position ‘M’. You have to reverse the array after that position.
+
+void reverseArray(vector<int> &arr , int m) {
+    int i = m+1;
+    int j = arr.size()-1;
+
+    while (i<j){
+        swap(arr[i], arr[j]);
+        i++;
+        j--;
+    }
+
+}
