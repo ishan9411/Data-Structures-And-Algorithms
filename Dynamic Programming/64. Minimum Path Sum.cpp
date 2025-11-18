cc80c4b3d237e6713202fa3e5b35b9fc7a528c6e
isha9411 <ishan9411@gmail.com>
2025-11-18 10:52:54 +0530

    added top down for min path sum

--- Dynamic Programming/64. Minimum Path Sum.cpp
+++ Dynamic Programming/64. Minimum Path Sum.cpp
@@ -24,3 +24,35 @@ public:
         return dp[m-1][n-1];
     }
 };
+
+//Solution 2 : Top-Down Approach
+
+class Solution {
+public:
+    int minSum(int i, int j, int m, int n, vector<vector<int>>& grid, vector<vector<int>>& dp){
+        if(i == m-1 && j == n-1) return grid[m-1][n-1];
+        
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int down = INT_MAX;
+        if(i+1 < m){
+            down   = minSum(i+1, j, m, n, grid, dp);
+        }
+        
+        int right = INT_MAX;
+        if(j+1 < n){
+            right  = minSum(i, j+1, m, n, grid, dp);
+        }
+        
+        return dp[i][j] = min(down, right) + grid[i][j];
+    }
+
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+
+        return minSum(0, 0, m, n, grid, dp);
+    }
+};
